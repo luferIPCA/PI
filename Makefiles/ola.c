12ccb3d85ea138c781cc69bcbd4f72f956e4bc47
ipcalp2 <lufer@ipca.pt>
2020-10-18 23:00:03 +0100

    inicio

---
+++ Makefiles/ola.c
@@ -0,0 +1,9 @@
+
+#include <stdio.h>
+
+#define IPCA "Instituto Politécnico do Cávado e do Ave"
+
+int main() {
+	printf("Escrevi algo....no Visual Studio 2019\n");
+	printf("%s\n",IPCA);
+}
---
+++ Teste/Hoje.c
@@ -0,0 +1,8 @@
+/**
+
+
+
+#include <stdio.h>
+int main() {
+    printf("Viva o ....\n");    /* ha quem diga que não...*/
+}
