b22e04dfa70bfde6cd06598fdf3625c1cc1c190e
lufer <lufer@ipca.pt>
2020-11-18 09:52:40 +0000

    Aula 16-11

---
+++ Aulas/Aula 16-11-2020/Aula.c
@@ -0,0 +1,23 @@
+/*****************************************************************//**
+ * \file   Aula.c
+ * \brief  
+ * 
+ * \author lufer
+ * \date   November 2020
+ *********************************************************************/
+#include <stdio.h>
+#include "Dados.h"
+#include "Funcoes.h"
+
+/**
+ * .
+ * 
+ * \return 
+ */
+int main() {
+
+	int aux = soma(2, 3);
+	printf("%d\n", aux);
+
+    printf("MAX = %d\n", MAX);
+}
